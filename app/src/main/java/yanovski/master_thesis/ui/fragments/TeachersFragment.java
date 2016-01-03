eb9ae47733a959d80ad32649904a574b08e2ef33
Samuil Yanovski <samuil.yanovski@gmail.com>
2016-01-03 22:57:04 +0200

    activity transitions

--- app/src/main/java/yanovski/master_thesis/ui/fragments/TeachersFragment.java
+++ app/src/main/java/yanovski/master_thesis/ui/fragments/TeachersFragment.java
@@ -2,14 +2,20 @@ package yanovski.master_thesis.ui.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import yanovski.master_thesis.R;
 import yanovski.master_thesis.data.LocalDataProvider;
 import yanovski.master_thesis.data.models.Teacher;
 import yanovski.master_thesis.ui.TeacherProfileActivity;
 import yanovski.master_thesis.ui.adapters.TeachersAdapter;
 import yanovski.master_thesis.ui.adapters.base.BaseRecyclerViewAdapter;
+import yanovski.master_thesis.ui.adapters.base.creators.TeacherVHCreator;
+import yanovski.master_thesis.ui.base.BaseActivity;
 import yanovski.master_thesis.ui.base.BaseListFragment;
 
 /**
@@ -30,8 +36,17 @@ public class TeachersFragment extends BaseListFragment implements
     public void onItemClick(BaseRecyclerViewAdapter<?> adapter, RecyclerView.ViewHolder holder,
         int position) {
         Teacher teacher = (Teacher) adapter.getItem(position);
+        TeacherVHCreator.TeacherViewHolder h = (TeacherVHCreator.TeacherViewHolder) holder;
+
+        BaseActivity activity = (BaseActivity) getActivity();
+        Pair<View, String> avatar =
+            Pair.create(h.avatar, getString(R.string.transition_avatar));
+        Pair<View, String> name = Pair.create(h.name, getString(R.string.transition_name));
+        ActivityOptionsCompat options =
+            ActivityOptionsCompat.makeSceneTransitionAnimation(activity, avatar, name);
+
         Intent intent = new Intent(getActivity(), TeacherProfileActivity.class);
         intent.putExtra(TeacherProfileActivity.KEY_TEACHER, teacher);
-        startActivity(intent);
+        ActivityCompat.startActivity(activity, intent, options.toBundle());
     }
 }
